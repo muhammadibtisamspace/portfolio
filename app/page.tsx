a5d98a94b2ae9421679bfad1dc258724ccfc04e8
Muhammad Ibtisam Babar <muhammadibtisamspace@gmail.com>
2026-06-07 11:43:01 +0500

    Comment out Testimonials component
    Comment out the Testimonials component in the main layout.

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <Hero />
         <About />
         <Projects />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
       <Footer />
